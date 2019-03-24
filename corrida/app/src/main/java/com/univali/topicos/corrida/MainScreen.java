d352795a883aa23cfd49a22cdce4cdaae1004b0b
Bianka Passos <biankatpas@gmail.com>
2019-03-24 20:17:48 -0300

    ex3 da lista 2

--- corrida/app/src/main/java/com/univali/topicos/corrida/MainScreen.java
+++ corrida/app/src/main/java/com/univali/topicos/corrida/MainScreen.java
@@ -1,13 +1,226 @@
 package com.univali.topicos.corrida;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
-public class MainScreen extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MainScreen extends AppCompatActivity
+{
+    boolean ASC = true;
+    boolean DESC = false;
+
+    ArrayList<EditText> atletas = new ArrayList<>();
+    ArrayList<EditText> tempos = new ArrayList<>();
+
+    Button btCalcular;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        atletas.add((EditText) findViewById(R.id.edA1));
+        atletas.add((EditText) findViewById(R.id.edA2));
+        atletas.add((EditText) findViewById(R.id.edA3));
+        atletas.add((EditText) findViewById(R.id.edA4));
+        atletas.add((EditText) findViewById(R.id.edA5));
+        atletas.add((EditText) findViewById(R.id.edA6));
+        atletas.add((EditText) findViewById(R.id.edA7));
+        atletas.add((EditText) findViewById(R.id.edA8));
+        atletas.add((EditText) findViewById(R.id.edA9));
+        atletas.add((EditText) findViewById(R.id.edA10));
+
+        tempos.add((EditText) findViewById(R.id.edT1));
+        tempos.add((EditText) findViewById(R.id.edT2));
+        tempos.add((EditText) findViewById(R.id.edT3));
+        tempos.add((EditText) findViewById(R.id.edT4));
+        tempos.add((EditText) findViewById(R.id.edT5));
+        tempos.add((EditText) findViewById(R.id.edT6));
+        tempos.add((EditText) findViewById(R.id.edT7));
+        tempos.add((EditText) findViewById(R.id.edT8));
+        tempos.add((EditText) findViewById(R.id.edT9)) ;
+        tempos.add((EditText) findViewById(R.id.edT10));
+
+        btCalcular = (Button) findViewById(R.id.btCalcular);
+        btCalcular.setEnabled(false);
+        btCalcular.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                boolean fieldsFilled=verificaCamposPreenchidos();
+                if(fieldsFilled)
+                    btCalcular.setEnabled(true);
+                else
+                    btCalcular.setEnabled(false);
+            }
+        });
+
     }
+
+    public boolean verificaCamposPreenchidos()
+    {
+        for(int i=0; i<atletas.size();i++)
+            if((atletas.get(i).getText().toString().equals("")) || tempos.get(i).getText().toString().equals(""))
+                return false;
+        return true;
+    }
+
+    public void calcular(View v)
+    {
+        Map<String, Calendar> corrida = getDadosCorrida();
+
+        String resultado = calcularMelhorTempo(corrida);
+        Calendar media = calcularMedia(corrida);
+        Calendar desvio = calcularDesvioPadrao(corrida);
+
+        Intent it = new Intent(this, Resultado.class);
+        it.putExtra("resultado", resultado);
+        String m = media.get(Calendar.HOUR_OF_DAY) +"H "+ media.get(Calendar.MINUTE) +"m "+ media.get(Calendar.SECOND) + "s";
+        it.putExtra("media", m);
+        String d = desvio.get(Calendar.HOUR_OF_DAY) +"H "+ desvio.get(Calendar.MINUTE) +"m "+ desvio.get(Calendar.SECOND) + "s";
+        it.putExtra("desvio", d);
+        startActivity(it);
+    }
+
+    public Map<String, Calendar> getDadosCorrida()
+    {
+        Map<String, Calendar> corrida = new HashMap<>();
+        for(int i=0; i<atletas.size();i++)
+        {
+            String nome = atletas.get(i).getText().toString();
+            Calendar tempo = stringToCalendar(tempos.get(i).getText().toString());
+            corrida.put(nome, tempo);
+        }
+        return corrida;
+    }
+
+    public Calendar stringToCalendar(String time)
+    {
+        String[] splitTime = time.split(":");
+        int h = Integer.parseInt(splitTime[0]);
+        int m = Integer.parseInt(splitTime[1]);
+        int s = Integer.parseInt(splitTime[2]);
+
+        Calendar c = new GregorianCalendar();
+        c.set(Calendar.HOUR_OF_DAY,h);
+        c.set(Calendar.MINUTE,m);
+        c.set(Calendar.SECOND,s);
+
+        return c;
+    }
+
+    public String calcularMelhorTempo(Map<String, Calendar> corrida)
+    {
+        Map<String, Calendar> sortedMapDesc = sortByComparator(corrida, ASC);
+        return getText(sortedMapDesc);
+    }
+
+    private Map<String, Calendar> sortByComparator(Map<String, Calendar> unsortMap, final boolean order)
+    {
+
+        List<Map.Entry<String, Calendar>> list = new LinkedList<>(unsortMap.entrySet());
+
+        // Sorting the list based on values
+        Collections.sort(list, new Comparator<Map.Entry<String, Calendar>>()
+        {
+            public int compare(Map.Entry<String, Calendar> o1,
+                               Map.Entry<String, Calendar> o2)
+            {
+                if (order)
+                {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+                else
+                {
+                    return o2.getValue().compareTo(o1.getValue());
+
+                }
+            }
+        });
+
+        // Maintaining insertion order with the help of LinkedList
+        Map<String, Calendar> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Calendar> entry : list)
+        {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedMap;
+    }
+
+    public String getText(Map<String, Calendar> map)
+    {
+        String m = "";
+        for (Map.Entry<String, Calendar> entry : map.entrySet()) {
+            m += "Atleta: " + entry.getKey() + " Tempo: " + entry.getValue().get(Calendar.HOUR_OF_DAY) + "H " + entry.getValue().get(Calendar.MINUTE) + "m "+ entry.getValue().get(Calendar.SECOND) +"s ,";
+        }
+        return m;
+    }
+
+    public Calendar calcularMedia(Map<String, Calendar> corrida)
+    {
+        Calendar total = new GregorianCalendar();
+        total.set(Calendar.HOUR_OF_DAY,0);
+        total.set(Calendar.MINUTE,0);
+        total.set(Calendar.SECOND,0);
+
+        for (Calendar tempo : corrida.values())
+        {
+            total.add(Calendar.HOUR_OF_DAY, tempo.get(Calendar.HOUR_OF_DAY));
+            total.add(Calendar.MINUTE, tempo.get(Calendar.MINUTE));
+            total.add(Calendar.SECOND, tempo.get(Calendar.SECOND));
+        }
+        int h = total.get(Calendar.HOUR_OF_DAY) / corrida.size();
+        int m = total.get(Calendar.MINUTE) / corrida.size();
+        int s = total.get(Calendar.SECOND) / corrida.size();
+
+        Calendar media = new GregorianCalendar();
+        media.set(Calendar.HOUR_OF_DAY, h);
+        media.set(Calendar.MINUTE, m);
+        media.set(Calendar.SECOND, s);
+        return media;
+    }
+
+    public Calendar calcularDesvioPadrao(Map<String, Calendar> corrida)
+    {
+        double totalH = 0;
+        double totalM = 0;
+        double totalS = 0;
+
+        Calendar media = calcularMedia(corrida);
+        for (Calendar tempo : corrida.values())
+        {
+            totalH += Math.pow(tempo.get(Calendar.HOUR_OF_DAY) - media.get(Calendar.HOUR_OF_DAY), 2);
+            totalM += Math.pow(tempo.get(Calendar.MINUTE) - media.get(Calendar.MINUTE), 2);
+            totalS += Math.pow(tempo.get(Calendar.SECOND) - media.get(Calendar.SECOND), 2);
+        }
+
+        double desvioH = Math.sqrt(totalH / corrida.size());
+        double desvioM = Math.sqrt(totalM / corrida.size());
+        double desvioS = Math.sqrt(totalS / corrida.size());
+
+        Calendar desvio = new GregorianCalendar();
+        desvio.set(Calendar.HOUR_OF_DAY, (int) Math.round(desvioH));
+        desvio.set(Calendar.MINUTE, (int) Math.round(desvioM));
+        desvio.set(Calendar.SECOND, (int) Math.round(desvioS));
+
+        return desvio;
+    }
+
 }
--- corrida/app/src/main/java/com/univali/topicos/corrida/Resultado.java
+++ corrida/app/src/main/java/com/univali/topicos/corrida/Resultado.java
@@ -1,13 +1,62 @@
 package com.univali.topicos.corrida;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
 
-public class Resultado extends AppCompatActivity {
+public class Resultado extends AppCompatActivity
+{
+
+    TextView tvResultado;
+    TextView tvMedia;
+    TextView tvDesvio;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        tvResultado = (TextView)findViewById(R.id.tvResultado);
+        tvMedia = (TextView)findViewById(R.id.tvMedia);
+        tvDesvio = (TextView)findViewById(R.id.tvDesvio);
+
+        Intent it = getIntent();
+        String[] resultado = it.getStringExtra("resultado").split("\\,", -1);
+        String media = it.getStringExtra("media");
+        String desvio = it.getStringExtra("desvio");
+
+        for(int i=0;i<resultado.length;i++)
+            if(i==0)
+                appendColoredText(tvResultado, resultado[i], Color.BLUE);
+            else
+                appendColoredText(tvResultado, resultado[i], Color.BLACK);
+
+        String m = "O tempo médio foi de " + media;
+        tvMedia.setText(m);
+        String d = "O desvio padrão foi de " + desvio;
+        tvDesvio.setText(d);
+
+    }
+
+    public void appendColoredText(TextView tv, String text, int color) {
+        int start = tv.getText().length();
+        tv.append(text);
+        int end = tv.getText().length();
+
+        Spannable spannableText = (Spannable) tv.getText();
+        spannableText.setSpan(new ForegroundColorSpan(color), start, end, 0);
+        tv.append("\n");
+    }
+
+    public void voltar(View v)
+    {
+        Intent i = new Intent(this, MainScreen.class);
+        startActivity(i);
     }
 }
