1822a35d4a01751e26a16c1aeffb799202040455
Bianka Passos <biankatpas@gmail.com>
2019-03-31 14:23:02 -0300

    ex3 lista 3

--- mensagens/app/src/main/java/com/univali/topicos/mensagens/MainScreen.java
+++ mensagens/app/src/main/java/com/univali/topicos/mensagens/MainScreen.java
@@ -1,7 +1,15 @@
 package com.univali.topicos.mensagens;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +17,40 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        String hora_atual = getHoraAtual();
+        verificaHorario(hora_atual);
+    }
+
+    private String getHoraAtual()
+    {
+        SimpleDateFormat dateFormat_hora = new SimpleDateFormat("HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        Date data_atual = cal.getTime();
+        return dateFormat_hora.format(data_atual);
+    }
+
+    private void verificaHorario(String hora_atual)
+    {
+        View view = this.getWindow().getDecorView();
+
+        String[] parts = hora_atual.split(":");
+        int hour = Integer.parseInt(parts[0]);
+
+        if (hour < 13)
+        {
+            Toast.makeText(this, "Bom dia!", Toast.LENGTH_LONG).show();
+            view.setBackgroundColor(Color.YELLOW);
+        }
+        else if (hour < 18)
+        {
+            Toast.makeText(this, "Boa tarde!", Toast.LENGTH_LONG).show();
+            view.setBackgroundColor(Color.GREEN);
+        }
+        else
+        {
+            Toast.makeText(this, "Boa Noite!", Toast.LENGTH_LONG).show();
+            view.setBackgroundColor(Color.BLUE);
+        }
     }
 }
