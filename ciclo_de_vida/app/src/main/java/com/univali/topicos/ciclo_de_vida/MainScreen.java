cc5fadfae3191cc140b005bbae5bb6444d94da8d
Bianka Passos <biankatpas@gmail.com>
2019-03-31 14:38:31 -0300

    ex2 lista 3

--- ciclo_de_vida/app/src/main/java/com/univali/topicos/ciclo_de_vida/MainScreen.java
+++ ciclo_de_vida/app/src/main/java/com/univali/topicos/ciclo_de_vida/MainScreen.java
@@ -2,6 +2,7 @@ package com.univali.topicos.ciclo_de_vida;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +10,58 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        String m = "Loading Activity";
+        System.out.println(m);
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String m = "Resuming Activity";
+        System.out.println(m);
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        String m = "Restarting Activity";
+        System.out.println(m);
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        String m = "Pausing Activity";
+        System.out.println(m);
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String m = "Destroying Activity";
+        System.out.println(m);
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String m = "Starting Activity";
+        System.out.println(m);
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        String m = "Stop Activity";
+        System.out.println(m);
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
     }
 }
--- mensagens/app/src/main/java/com/univali/topicos/mensagens/MainScreen.java
+++ mensagens/app/src/main/java/com/univali/topicos/mensagens/MainScreen.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
