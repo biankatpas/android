ebc6e542f21ba1b1785a1e91e99c80f594d91b57
Bianka Passos <biankatpas@gmail.com>
2019-06-10 00:01:30 -0300

    excluir pedidos

--- ConsumirAndroid/app/src/main/java/com/example/biankatpas/consumirandroid/AcessoRest.java
+++ ConsumirAndroid/app/src/main/java/com/example/biankatpas/consumirandroid/AcessoRest.java
@@ -126,4 +126,37 @@ public class AcessoRest {
 
         thread.start();
     }
+
+    public void sendDelete(final String urlAdress, final String json) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(urlAdress);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("DELETE");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    Log.i("JSON", json);
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    os.writeBytes(json);
+
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
 }
--- ConsumirAndroid/app/src/main/java/com/example/biankatpas/consumirandroid/ExcluirPedidoActivity.java
+++ ConsumirAndroid/app/src/main/java/com/example/biankatpas/consumirandroid/ExcluirPedidoActivity.java
@@ -1,13 +1,53 @@
 package com.example.biankatpas.consumirandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 
 public class ExcluirPedidoActivity extends AppCompatActivity {
+
+    String id;
+    String data;
+    String hora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excluir_pedido);
+
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+        data = intent.getStringExtra("data");
+        hora = intent.getStringExtra("hora");
+
+        TextView tvId = findViewById(R.id.tvId);
+        tvId.setText("Id: " + id);
+        TextView tvData = findViewById(R.id.tvData);
+        tvData.setText("Data: " + data);
+        TextView tvHora = findViewById(R.id.tvHora);
+        tvHora.setText("Hora: " + hora);
+    }
+
+    public void cancelarPedido(View v){
+        PedidoCompra p = new PedidoCompra();
+        p.setId(Long.parseLong(id));
+        Log.i("excluir", p.getId().toString());
+
+        Gson g = new Gson();
+        Type itemType = new TypeToken<PedidoCompra>() {}.getType();
+        String content = g.toJson(p, itemType);
+
+        AcessoRest ar = new AcessoRest();
+        ar.sendDelete("http://192.168.0.105:8080/Produto-WS/webresources/generic/pedidocompra/excluir", content);
+        Intent intent = new Intent(ExcluirPedidoActivity.this, HistoricoActivity.class);
+        startActivity(intent);
     }
 }
